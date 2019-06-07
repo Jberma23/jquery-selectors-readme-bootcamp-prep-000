f16e589e574a2989a92977d8c8f7f723015b3da4
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-07 21:03:12 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,15 +1,7 @@
 // declare your functions here...
 'use-strict';
-$(document.ready(function(){
+
 function paragraphSelector(){
  return $('p')
 
 }
-
-function lastImageSelector(){
-  var lastImageSelector = $('img: last');
-  return lastImageSelector
-}
-});
-$(document).ready(function(){
-  $("#text").append("this is so freaking cool.");
